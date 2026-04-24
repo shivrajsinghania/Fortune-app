34e980e549aa87888f57fc5b6d9687b56a7bbe50
Shivraj singhania <shivrajsinghania766@gmail.com>
2026-04-24 17:21:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import cloudinary
 import cloudinary.uploader
 
 cloudinary.config(
-    cloud_name=os.environ.get("CLOUD_NAME"),
-    api_key=os.environ.get("API_KEY"),
-    api_secret=os.environ.get("API_SECRET")
+    cloud_name="djvrsirrt",
+    api_key="241599576617161",
+    api_secret="g9BHmdK6OwM2HSVsdi4wWgq5ABo"
 )
 
 from werkzeug.utils import secure_filename
