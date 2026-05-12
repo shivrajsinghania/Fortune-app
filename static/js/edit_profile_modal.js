9eea20e7dbe17f419c7951cf9cd17118aa7b0320
Shivraj singhania <shivrajsinghania766@gmail.com>
2026-05-12 12:10:35 +0530

    Update edit_profile_modal.js

--- static/js/edit_profile_modal.js
+++ static/js/edit_profile_modal.js
@@ -2,10 +2,12 @@
 const modal = document.getElementById("editModal");
 const modalContent = modal.querySelector(".modal-content");
 const modalBody = modal.querySelector(".modal-body");
+const uploadLoader = document.getElementById("uploadLoader");
 
 // APP STATE
 let appState = {
-  modalOpen: false
+  modalOpen: false,
+  compressedProfileImage: null
 };
 
 // RENDER FUNCTION
@@ -23,6 +25,19 @@ function render() {
   }
 }
 
+function showLoader() {
+  uploadLoader.classList.add("show");
+}
+
+function hideLoader() {
+  uploadLoader.classList.remove("show");
+}
+
+function updatePreviewShape(shape) {
+  const previewImg = document.getElementById("previewImg");
+  previewImg.className = shape;
+}
+
 // OPEN MODAL
 function openEditModal() {
 
@@ -33,8 +48,7 @@ function openEditModal() {
 
   history.pushState(
     { modalOpen: true },
-    "",
-    "#edit"
+    ""
   );
 
   render();
@@ -45,6 +59,7 @@ function closeEditModal() {
 
   // close through history
   if (appState.modalOpen) {
+    appState.compressedProfileImage = null;
     history.back();
   }
 }
@@ -64,22 +79,134 @@ document
   .addEventListener("submit", async function (e) {
 
     e.preventDefault();
+    
+    showLoader();
+    
+    try {
+      const formData = new FormData();
+      
+      formData.append(
+        "name",
+        this.name.value
+      );
+      
+      formData.append(
+        "bio",
+        this.bio.value
+      );
+      
+      formData.append(
+        "links",
+        this.links.value
+      );
+      
+      formData.append(
+        "shape",
+        this.shape.value
+      );
+      
+      formData.append(
+        "fit_type",
+        this.fit_type.value
+      );
+      
+      //comopressed image 
+      if (appState.compressedProfileImage) {
+        formData.append(
+          "profile_pic",
+          appState.compressedProfileImage
+        );
+      }
+      
+      const response = await fetch("/profile/update", {
+        method: "POST",
+        body: formData
+      });
+      
+      const data = await response.json();
+      
+      await preloadImage(data.profile_pic);
+      updateProfileUI(data);
+      hideLoader();
+      closeEditModal();
+      
+    } catch (error) {
+      hideLoader();
+      alert("upload failed!")
+    }
+  });
 
-    const formData = new FormData(this);
+async function compressImage(file) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    img.onload = () => {
+      const canvas =
+        document.createElement("canvas");
+        
+      const ctx =
+        canvas.getContext("2d");
+        
+      const maxWidth = 600;
+      let width = img.width;
+      let height = img.height;
 
-    const response = await fetch("/profile/update", {
-      method: "POST",
-      body: formData
-    });
+      // maintain ratio
+      if (width > maxWidth) {
 
-    const data = await response.json();
+        height =
+          (height * maxWidth) / width;
 
-    // live UI update
-    updateProfileUI(data);
+        width = maxWidth;
+      }
 
-    // close modal
-    closeEditModal();
-});
+      canvas.width = width;
+      canvas.height = height;
+
+      ctx.drawImage(
+        img,
+        0,
+        0,
+        width,
+        height
+      );
+
+      canvas.toBlob(
+
+        (blob) => {
+
+          const compressedFile =
+            new File(
+              [blob],
+              file.name,
+              {
+                type: "image/jpeg"
+              }
+            );
+
+          resolve(compressedFile);
+        },
+        "image/jpeg",
+        0.7
+      );
+    };
+  });
+}
+
+function preloadImage(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = src;
+    
+    img.onload = () => {
+      resolve();
+    };
+    
+    img.onerror = () => {
+      reject();
+    };
+  });
+}
 
 // UPDATE PROFILE UI
 function updateProfileUI(data) {
@@ -98,6 +225,7 @@ function updateProfileUI(data) {
 
   img.src = data.profile_pic;
   img.className = data.shape;
+  document.getElementById("previewImg").className = data.shape;
   img.style.objectFit = data.fit_type;
 
   // LINKS
@@ -136,13 +264,28 @@ function updateProfileUI(data) {
 }
 
 // IMAGE PREVIEW
-function previewImage(event) {
-
-  const img =
-    document.getElementById("previewImg");
-
-  img.src =
-    URL.createObjectURL(event.target.files[0]);
+async function previewImage(event) {
+  const file = event.target.files[0];
+  
+  //compress IMAGE
+  appState.compressedProfileImage = await compressImage(file);
+  
+  //preview compressed image 
+  const img = document.getElementById("previewImg");
+  
+  img.src = URL.createObjectURL(appState.compressedProfileImage);
+  
+  console.log(
+    "Original:",
+    (file.size / 1024/ 1024).toFixed(2),
+    "MB"
+  );
+  
+  console.log(
+    "Compressed:",
+    (appState.compressedProfileImage.size / 1024).toFixed(0),
+    "KB"
+  );
 }
 
 // SWIPE TO CLOSE
