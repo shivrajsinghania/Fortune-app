32a0a22ed077b16579af20efa23288bd6c779739
Shivraj singhania <shivrajsinghania766@gmail.com>
2026-05-22 11:03:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -850,4 +850,4 @@ def handle_send_message(data):
 # ================== RUN ==================
 port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_wekzeug=True)
