0b2e6e57b195753d5a4b7cafb8bdb1e09c222343
Shivraj singhania <shivrajsinghania766@gmail.com>
2026-06-08 04:34:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1363,3 +1363,4 @@ def get_connections(user_id, mode):
 port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+	
